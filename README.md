b4641839ae83dac9794947bac4b46d641bae6461
Laoujin <woutervs@hotmail.com>
2018-08-24 21:59:17 +0200

    new purpose: send ip to start sniffing the target out

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 Jaen The Unbeliever
 ===================
 
-Heh heh.
+Wait on port 3000 for ip to passed and start hax0ring 'em!
+
+```
+npm install
+npm start
+```
+
+And visit
+```
+http://localhost:3000/?ip=127.0.0.1
+```
--- doIt.js
+++ doIt.js
@@ -1,8 +1,11 @@
-var express = require('express');
-var app = express();
+const shell = require('shelljs');
+const express = require('express');
+const app = express();
 
 app.get('/', function(request, response) {
-   response.send("This would be some HTML");
+  const ipToHack = request.query.ip;
+  response.send('Start hacking ' + ipToHack);
+  shell.exec('./path_to_hax0r_tools/hackem.sh');
 });
 
 app.listen(3000);
